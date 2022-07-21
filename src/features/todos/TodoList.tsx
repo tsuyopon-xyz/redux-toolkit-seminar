42927e7b1a551e2c1be30e56c89848c58de93ef3
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-07-21 10:52:50 +0900

    Separated TodoTable from TodoList.tsx

--- src/features/todos/TodoList.tsx
+++ src/features/todos/TodoList.tsx
@@ -1,9 +1,7 @@
 import { FC } from 'react';
 import { useTodos, DISPLAY_FLAG_MAP, DisplayFlagType } from './useTodos';
-import { useConfirmModal } from './modals/ConfirmModal/useConfirmModal';
-import { useUpdateTodoModal } from './modals/UpdateTodoModal/useUpdateTodoModal';
-import { translateStatus } from './utils/todo-converter';
 import { TodoForm } from './TodoForm';
+import { TodoTable } from './TodoTable';
 
 export const TodoList: FC = () => {
   const {
@@ -16,23 +14,11 @@ export const TodoList: FC = () => {
     removeTodo,
     restoreTodo,
   } = useTodos();
-  const {
-    open: openConfirmModal,
-    setMessage,
-    ConfirmModalWrapper,
-  } = useConfirmModal();
-  const {
-    open: openUpdateTodoModal,
-    setTodoInput: setTodoInputForUpdateTodoModal,
-    UpdateTodoModalWrapper,
-  } = useUpdateTodoModal();
 
   if (isLoading) return <div>読み込み中...</div>;
 
   return (
     <div>
-      <ConfirmModalWrapper />
-      <UpdateTodoModalWrapper />
       <TodoForm onSubmit={(input) => addTodo(input)} />
       <hr />
       <div>
@@ -51,83 +37,22 @@ export const TodoList: FC = () => {
         </select>
       </div>
 
-      <table border={1}>
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>タイトル</th>
-            <th>本文</th>
-            <th>ステータス</th>
-            <th>作成日時</th>
-            <th>更新日時</th>
-            <th>削除日時</th>
-            <th>更新ボタン</th>
-            <th>
-              {displayFlag === 'deleted' ? '削除取り消しボタン' : '削除ボタン'}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {todos.length === 0 ? (
-            <tr>
-              <td colSpan={9} style={{ textAlign: 'center' }}>
-                データなし
-              </td>
-            </tr>
-          ) : (
-            todos.map((todo) => {
-              return (
-                <tr key={todo.id}>
-                  <td>{todo.id}</td>
-                  <td>{todo.title}</td>
-                  <td>{todo.body}</td>
-                  <td>{translateStatus(todo.status)}</td>
-                  <td>{todo.createdAt}</td>
-                  <td>{todo.updatedAt ?? '無し'}</td>
-                  <td>{todo.deletedAt ?? '無し'}</td>
-                  <td>
-                    <button
-                      disabled={displayFlag === 'deleted'}
-                      onClick={() => {
-                        setTodoInputForUpdateTodoModal(todo);
-                        openUpdateTodoModal((newTodoInput) => {
-                          updateTodo({
-                            id: newTodoInput.id!,
-                            input: newTodoInput,
-                          });
-                        });
-                      }}
-                    >
-                      更新
-                    </button>
-                  </td>
-                  <td>
-                    {displayFlag === 'deleted' ? (
-                      <button
-                        onClick={() => {
-                          setMessage('削除を取り消しますか？');
-                          openConfirmModal(() => restoreTodo(todo.id));
-                        }}
-                      >
-                        削除取り消し
-                      </button>
-                    ) : (
-                      <button
-                        onClick={() => {
-                          setMessage('本当に削除しますか？');
-                          openConfirmModal(() => removeTodo(todo.id));
-                        }}
-                      >
-                        削除
-                      </button>
-                    )}
-                  </td>
-                </tr>
-              );
-            })
-          )}
-        </tbody>
-      </table>
+      <TodoTable
+        todos={todos}
+        displayFlag={displayFlag}
+        onUpdate={(input) => {
+          updateTodo({
+            id: input.id!,
+            input,
+          });
+        }}
+        onRemove={(id) => {
+          removeTodo(id);
+        }}
+        onRestore={(id) => {
+          restoreTodo(id);
+        }}
+      />
     </div>
   );
 };
